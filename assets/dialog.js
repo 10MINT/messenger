4a08882f5bfbac5c87dbf777784e7c7fddbd49a0
10MINT <redpandadevs@gmail.com>
2019-10-15 00:11:45 +0200

    assets updated

--- assets/dialog.js
+++ assets/dialog.js
@@ -1,5 +1,5 @@
+window.onload = function() {scrollFunction();toBottom();linkImages();};
 window.onscroll = function() {scrollFunction()};
-
 function scrollFunction() {
     if ((window.innerHeight + window.scrollY) >= document.body.scrollHeight) {
         document.getElementById("tobottom").style.display = "none";
@@ -7,11 +7,24 @@ function scrollFunction() {
         document.getElementById("tobottom").style.display = "block";
     }
 }
+function toBottom(){window.scrollTo(0,document.body.scrollHeight);}
+function toTop(){window.scrollTo(0,0);}
 
-function toBottom(){
-    window.scrollTo(0,document.body.scrollHeight);
+function linkImages(){
+	document.getElementsByTagName("head")[0].innerHTML += '\n		<link rel="stylesheet" href="file:///android_asset/modal.css" />';
+	document.getElementsByTagName("head")[0].innerHTML += '\n		<link rel="stylesheet" href="modal.css" />'; //for Companion
+	document.getElementsByTagName("body")[0].innerHTML += '		<div id="modal" class="modal"><span class="close">&times;</span><img class="modal-content" id="modal-img"></div>';
+	document.getElementsByClassName("close")[0].onclick = function() {
+		document.getElementById('modal').style.display = "none";
+	};
+	const l = document.getElementsByTagName("img");
+	for (let i = 0; i < l.length; i++) {
+		l[i].onclick = function(){
+			document.getElementById('modal').style.display = "block";
+			document.getElementById('modal-img').src = l[i].src;
+			// document.location.href = l[i].src;
+			// console.log("Image clicked");
+		}
+	};
 }
-
-function toTop(){
-    window.scrollTo(0,0);
-}
+		
